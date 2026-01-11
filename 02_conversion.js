84e1be7a9f6c572d8646e1ef67346c4c9943bf0a
Yusufzai Fawwad <er.fawwad@gmail.com>
2026-01-11 22:39:50 +0530

    convertion of data types

---
+++ 02_conversion.js
@@ -0,0 +1,37 @@
+// Converting String to Number using Number()
+
+let score = "44FortyFour"; // Number
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+
+// Converting String to Number
+let str = "324";
+console.log(typeof str);
+
+// "44" -> 44 using Number()
+let str1 = "44";
+let num = Number(str);
+console.log(typeof num);
+console.log(num);
+
+// Converting Boolean
+
+let isLoggedIn = "true";
+console.log(typeof isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// Converting Number to String
+
+let marks = 34;
+console.log(typeof marks);
+let marksInString = String(marks);
+console.log(typeof marksInString);
+console.log(marksInString);
