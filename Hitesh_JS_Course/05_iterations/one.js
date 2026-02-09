c7083db93fe9d2be1617c6c662b05dfe73283d57
Yusufzai Fawwad <er.fawwad@gmail.com>
2026-02-09 22:27:36 +0530

    09th Feb

--- Hitesh_JS_Course/05_iterations/one.js
+++ Hitesh_JS_Course/05_iterations/one.js
@@ -9,6 +9,18 @@ for (let i = 0; i <= 10; i++) {
     
 }
 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("5 is best number");
+//         break
+//     }
+//     console.log(element);
+    
+// } written by Fawwad
+
+
+
 // console.log(element);
 
 for (let i = 1; i <= 10; i++) {
